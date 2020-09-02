c79e2e06ac71ccc9f67ddc893e05592d0bf41b8b
Oleksiy Oletsky <oletsky@ukr.net>
2020-09-02 14:19:32 +0300

    Add files via upload

---
+++ dynamicinsert.js
@@ -0,0 +1,11 @@
+"use strict";
+let mas=[];
+mas[0]=10;
+mas[1]=20;
+console.log("Length is "+mas.length); //2
+console.log(mas);
+//[ 10, 20 ]
+mas[5]=100;
+console.log("Length is "+mas.length); //6
+console.log(mas);
+//[ 10, 20, <3 empty items>, 100 ]
