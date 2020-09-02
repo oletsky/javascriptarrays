7bfcf9789fde14dc04ce2c5377d8136fd2a4a78c
Oleksiy Oletsky <oletsky@ukr.net>
2020-09-02 13:52:55 +0300

    Add files via upload

---
+++ addingelements.js
@@ -0,0 +1,14 @@
+"use strict";
+let mas=[10, 20, 30];
+mas.push(50);
+console.log(mas);
+//[ 10, 20, 30, 50 ]
+mas.unshift(1000);
+console.log(mas);
+//[ 1000, 10, 20, 30, 50 ]
+mas.pop();
+console.log(mas);
+//[ 1000, 10, 20, 30 ]
+mas.shift();
+console.log(mas);
+[ 10, 20, 30 ]
