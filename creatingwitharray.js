fa28f077fba05ca3101c46774c8d999803fd380e
Oleksiy Oletsky <oletsky@ukr.net>
2020-09-02 09:32:30 +0300

    Add files via upload

---
+++ creatingwitharray.js
@@ -0,0 +1,10 @@
+"use strict";
+let mas1= new Array(1,2,3);
+console.log("Length is "+mas1.length); //3
+console.log(mas1); //[ 1, 2, 3 ]
+let mas2= new Array(3);
+console.log("Length is "+mas2.length); //3
+console.log(mas2[1]);
+let mas3= new Array([3]);
+console.log("Length is "+mas3.length); //3
+console.log(mas3[0]);
