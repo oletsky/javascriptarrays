724736a6c482049bbc168a01765df9d23c817c10
Oleksiy Oletsky <oletsky@ukr.net>
2020-09-02 15:16:04 +0300

    Add files via upload

---
+++ manipulatinglength.js
@@ -0,0 +1,12 @@
+"use strict";
+let mas=[10,20,30,40,50];
+console.log(mas.length);
+console.log(mas);
+mas.length=3;
+console.log(mas.length);//3
+console.log(mas);
+//[ 10, 20, 30 ]
+mas.length=5;
+console.log(mas.length)//5
+console.log(mas);
+//[ 10, 20, 30, <2 empty items> ]
